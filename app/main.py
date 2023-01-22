9f2010dd50d84197461a7bdf975ddad8ced17666
Hunter Jones <hjones82493@gmail.com>
2023-01-21 21:51:35 -0600

    Black changes

--- app/main.py
+++ app/main.py
@@ -4,24 +4,22 @@ from flask import Flask
 
 from routes import main_page
 
+
 def create_app():
-	app = Flask(__name__)
-	app.register_blueprint(main_page.home_page)
+    app = Flask(__name__)
+    app.register_blueprint(main_page.home_page)
 
-	return app
+    return app
 
-if __name__ == '__main__':
-	host_name = os.environ.get('FLASK_HOST', '127.0.0.1')
-	port = os.environ.get('FLASK_PORT', '5000')
-	environment = os.environ.get('FLASK_ENV', 'dev')
-	
-	debug = False
-	if environment == 'dev':
-		debug = True
-	
-	run_app = create_app()
-	run_app.run(
-		host=host_name,
-		port=port, 
-		debug=debug
-	)
+
+if __name__ == "__main__":
+    host_name = os.environ.get("FLASK_HOST", "127.0.0.1")
+    port = os.environ.get("FLASK_PORT", "5000")
+    environment = os.environ.get("FLASK_ENV", "dev")
+
+    debug = False
+    if environment == "dev":
+        debug = True
+
+    run_app = create_app()
+    run_app.run(host=host_name, port=port, debug=debug)
--- app/routes/main_page.py
+++ app/routes/main_page.py
@@ -1,11 +1,12 @@
 from flask import Blueprint, render_template, abort
 from jinja2 import TemplateNotFound
 
-home_page = Blueprint('home_page', __name__, template_folder='../templates')
+home_page = Blueprint("home_page", __name__, template_folder="../templates")
 
-@home_page.route('/', methods=['GET'])
+
+@home_page.route("/", methods=["GET"])
 def show():
-	try:
-		return render_template('index.html')
-	except TemplateNotFound:
-		abort(404)
+    try:
+        return render_template("index.html")
+    except TemplateNotFound:
+        abort(404)
