b49d39b859798eeae2e98153f772f23f4bc32797
Hunter Jones <hjones82493@gmail.com>
2023-01-21 21:46:37 -0600

    Update to use env variables for running

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 
 from routes import main_page
@@ -9,5 +11,17 @@ def create_app():
 	return app
 
 if __name__ == '__main__':
+	host_name = os.environ.get('FLASK_HOST', '127.0.0.1')
+	port = os.environ.get('FLASK_PORT', '5000')
+	environment = os.environ.get('FLASK_ENV', 'dev')
+	
+	debug = False
+	if environment == 'dev':
+		debug = True
+	
 	run_app = create_app()
-	run_app.run(debug=True)
+	run_app.run(
+		host=host_name,
+		port=port, 
+		debug=debug
+	)
