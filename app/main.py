1e6f20d2ab127c4c14606c5936e0aa9889064823
Hunter Jones <hjones82493@gmail.com>
2023-01-23 23:36:21 -0600

    Add context logic for social links

--- app/main.py
+++ app/main.py
@@ -2,13 +2,29 @@ import os
 
 from flask import Flask
 
-from routes import main_page
-
+from routes import main_page, gallery_page, order_page, blog_page
 
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(main_page.home_page)
+    app.register_blueprint(gallery_page.gallery_page)
+    app.register_blueprint(order_page.order_page)
+    app.register_blueprint(blog_page.blog_page)
 
+    socials = {
+        'Facebook' : 'https://www.facebook.com',
+        'Instagram' : 'https://www.instagram.com',
+        'Pinterest' : 'https://www.pinterest.com'
+    }
+
+    @app.context_processor
+    def inject_socials():
+        return dict(
+            Facebook=socials.get('Facebook'),
+            Instagram=socials.get('Instagram'),
+            Pinterest=socials.get('Pinterest')
+        )
+        
     return app
 
 
@@ -16,6 +32,8 @@ if __name__ == "__main__":
     host_name = os.environ.get("FLASK_HOST", "127.0.0.1")
     port = os.environ.get("FLASK_PORT", "5000")
     environment = os.environ.get("FLASK_ENV", "dev")
+
+    print(host_name, port, environment)
 
     debug = False
     if environment == "dev":
